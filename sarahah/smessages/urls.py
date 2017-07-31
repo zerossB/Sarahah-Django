4d8d6ab0b7800f92957c98b313b597fc431068ac
Haynesss <zerossb@gmail.com>
2017-07-31 16:05:45 -0300

    Delete messages

--- sarahah/smessages/urls.py
+++ sarahah/smessages/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 
-from .views import favoriteMessage
+from .views import favoriteMessage, deleteMessage
 
 
 urlpatterns = [
     url(r'favorite/(?P<idMessage>[\w0-9_-]+)/', favoriteMessage, name='favorite'),
+    url(r'delete/(?P<idMessage>[\w0-9_-]+)/', deleteMessage, name='delete'),
 ]
--- sarahah/smessages/views.py
+++ sarahah/smessages/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Messages
 
 # Create your views here.
+
+
 def favoriteMessage(request, idMessage):
     if request.method == "POST":
         message = get_object_or_404(Messages, pk=idMessage)
         favr = message.favorite
-        print( message.favorite )
+        print(message.favorite)
         message.favorite = not favr
-        print( message.favorite )
+        print(message.favorite)
         message.save()
+        return redirect("account:dashboard")
+    else:
+        return redirect("account:dashboard")
+
+
+def deleteMessage(request, idMessage):
+    if request.method == "POST":
+        message = get_object_or_404(Messages, pk=idMessage)
+        message.delete()
         return redirect("account:dashboard")
     else:
         return redirect("account:dashboard")
